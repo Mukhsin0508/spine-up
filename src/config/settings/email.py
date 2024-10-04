e371e535a30eab0e5c507013d4517fe181614b0d
Mukhsin0508 <mukhsinmukhtorov@arizona.edu>
2024-10-04 07:08:16 +0500

    Refactor: added anti-virus validations to resume, and updated the SMTP server configurations -> email sending working.

--- src/config/settings/email.py
+++ src/config/settings/email.py
@@ -1,11 +1,14 @@
+import mimetypes
 from django.core.mail import EmailMessage
 from django.conf import settings
 
+from config.validators import scan_file_for_viruses
+
 
 def send_application_email(application):
-    subject = f"New application for {application.vacancy.title}"
+    subject = f"New application for {application.vacancy.title} from {application.name}"
     message = f"""
-    New Application recieved:
+    New Application received:
     Name: {application.name}
     Phone: {application.phone_number}
     Experience: {application.experience}
@@ -17,6 +20,27 @@ def send_application_email(application):
         settings.EMAIL_HOST_USER,
         [settings.EMAIL_HR_USER]
         )
-    email.attach(application.resume.name, application.resume.read(), application.resume.content_type)
+
+    if application.resume:
+        # ==== Get the file name ====
+        file_name = application.resume.name.split('/')[-1]
+        file_path = application.resume.path
+
+        # ==== Scan the uploaded resume ====
+        scan_file_for_viruses(file_path)
+
+        # Determine content type based on the file extension and encoding
+        content_type, encoding = mimetypes.guess_type(file_name)
+        if not content_type:
+            # ===== Default to binary if type can't be guessed
+            content_type = 'application/octet-stream'
+
+        # ==== Attach the resume to the email ====
+        email.attach(
+            file_name,
+            application.resume.read(),
+            content_type
+        )
+
     email.send()
 
